c356b9269300a491fa2532028282e4ec7120078a
Christopher Best <bestchris@gmail.com>
2013-05-20 12:13:37 -0400

    use maxlength and setRangeText to simplify paste handler set maxlength attribute to current length on creation

--- overwrite.dart
+++ overwrite.dart
@@ -4,36 +4,15 @@ part of Tabasci;
 class Overwrite {
   // the element to implement overwrite mode in
   TextAreaElement _element;
-  // the length of the text in the element
-  int _length;
   
   /// Create an overwrite object that implements overwrite mode on the input element 
   Overwrite(TextAreaElement this._element) {
-    _length = _element.value.length;
+    // set max length of element text to current length
+    // TODO this should be set more intelligently
+    _element.maxLength = _element.value.length;
     _element.onPaste.listen((Event e) {
-      // save the cursor location
-      int cursor = _element.selectionStart;
-      // get the string to be pasted
-      String pasteString = e.clipboardData.getData("Text");
-      // truncate clipboard data if it is too long
-      if(pasteString.length > _length - _element.selectionStart) {
-        pasteString = pasteString.substring(0, _length - _element.selectionStart);
-      }
-      // get prefix of existing value, everything up to cursor
-      String prefix = _element.value.substring(0,_element.selectionStart);
-      // get suffix of existing value, everything after the suffx and the length of the paste string
-      String suffix = "";
-      if(pasteString.length < _length - cursor) {
-        suffix = _element.value.substring(_element.selectionStart + pasteString.length);
-      }
-      // set new contents value
-      _element.value = "$prefix$pasteString$suffix";
-      // stop paste action from going through
-      // NOTE we only strictly need to do this if clipboard contents are too long for input,
-      // NOTE but it is simpler to do it for both cases
-      e.preventDefault();
-      // restore cursor
-      _element.selectionEnd = _element.selectionStart = cursor;
+      // remove enough characters to make room for pasted text
+      _element.setRangeText("", _element.selectionStart, _element.selectionStart + e.clipboardData.getData("Text").length, "start");
     });
     _element.onCut.listen((Event e) {
       // create a string of spaces the same length as the text that will be cut
@@ -45,7 +24,7 @@ class Overwrite {
     });
     _element.onKeyDown.listen((Event e) {
       Logger.root.info("overwrite got key down which: ${e.which}");
-      Logger.root.info("_length: $_length, new length: ${_element.value.length}");
+      Logger.root.info("_length: ${_element.maxLength}, new length: ${_element.value.length}");
       if(_element.selectionStart == _element.selectionEnd) { 
         if(e.which == 8) {
           // don't do anything if cursor is at the end
@@ -55,7 +34,7 @@ class Overwrite {
           }
         } else if(e.which == 46) {
           // don't do anything if cursor is at the end
-          if(_element.selectionStart != _length) {
+          if(_element.selectionStart != _element.maxLength) {
             // insert a space after the character that will be deleted by the delete
             _element.setRangeText(" ", _element.selectionStart+1, _element.selectionStart+1);
           }
@@ -74,8 +53,8 @@ class Overwrite {
     _element.onKeyPress.listen((Event e) {
       Logger.root.info("overwrite got key press");
       // if cursor is at end, move it back one
-      if(_element.selectionStart == _length) {
-        _element.selectionEnd = _element.selectionStart = _length - 1;
+      if(_element.selectionStart == _element.maxLength) {
+        _element.selectionEnd = _element.selectionStart = _element.maxLength - 1;
       }
       // save the cursor position
       int cursor = _element.selectionStart;
