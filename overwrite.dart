9b93e946af2b325269f1fc7fe9b2cff8ab0680e2
Christopher Best <bestchris@gmail.com>
2013-05-19 22:15:32 -0400

    handle backspace

--- overwrite.dart
+++ overwrite.dart
@@ -10,10 +10,29 @@ class Overwrite {
   /// Create an overwrite object that implements overwrite mode on the input element 
   Overwrite(TextAreaElement this._element) {
     _length = _element.value.length;
+    _element.onKeyDown.listen((Event e) {
+      Logger.root.info("overwrite got key down which: ${e.which}");
+      Logger.root.info("_length: $_length, new length: ${_element.value.length}");
+      if(e.which == 8) {
+        Logger.root.info("which is backspace, adding a space back in");
+        // save the cursor position
+        int cursor = _element.selectionStart;
+        // set the value to the existing value with a space in the place after selection start
+        _element.value = "${_element.value.substring(0, _element.selectionStart)} ${_element.value.substring(_element.selectionStart)}";
+        // restore cursor
+        _element.selectionEnd = _element.selectionStart = cursor;
+        // NOTE the backspace will still go through and delete the character before the selection start
+        // NOTE an alternative is to take out this char ourselves and e.preventDefault() to stop the backspace
+      }
+    });
     _element.onKeyPress.listen((Event e) {
+      Logger.root.info("overwrite got key press");
       if(_element.value.length == _length + 1) {
-        int cursor = _element.selectionStart; 
+        // save the cursor position
+        int cursor = _element.selectionStart;
+        // set value to prefix + new char + suffix
         _element.value = _element.value.substring(0, _element.selectionStart) + _element.value.substring(_element.selectionStart + 1);
+        // restore cursor
         _element.selectionEnd = _element.selectionStart = cursor;
       }
     });
