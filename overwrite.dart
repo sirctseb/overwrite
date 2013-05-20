286ea2bce29e27ef635c57aa928e26e3297d5623
Christopher Best <bestchris@gmail.com>
2013-05-19 21:52:06 -0400

    handle single character entry case

--- overwrite.dart
+++ overwrite.dart
@@ -1,2 +1,26 @@
 /** overwrite.dart implements overwrite mode in a text input element */
 part of Tabasci;
+
+class Overwrite {
+  // the element to implement overwrite mode in
+  TextAreaElement _element;
+  // the length of the text in the element
+  int _length;
+  // the text on the element
+  String _text;
+  String _newText;
+  bool _addChar = false;
+  int _cursor;
+  
+  /// Create an overwrite object that implements overwrite mode on the input element 
+  Overwrite(TextAreaElement this._element) {
+    _length = _element.value.length;
+    _element.onKeyPress.listen((Event e) {
+      if(_element.value.length == _length + 1) {
+        int cursor = _element.selectionStart; 
+        _element.value = _element.value.substring(0, _element.selectionStart) + _element.value.substring(_element.selectionStart + 1);
+        _element.selectionEnd = _element.selectionStart = cursor;
+      }
+    });
+  }
+}
